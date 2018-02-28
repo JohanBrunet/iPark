a75b42b44875faff1d444d9283ee12e2a6bedadc
Jade HENNEBERT <jade.hennebert@igtp4-mac07.isim.intra>
2018-02-28 08:21:25 +0100

    init project

---
+++ iPark/iPark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPark
+//
+//  Created by Jade HENNEBERT on 28/02/2018.
+//  Copyright © 2018 Jade HENNEBERT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
