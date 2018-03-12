dc55a834050282c568980725fc86de201761bda1
Johan Brunet <brunet.johan.23@gmail.com>
2018-03-12 22:47:32 +0100

    Add data manager + model + controller

--- iPark/iPark/controllers/FirstLaunchViewController.swift
+++ iPark/iPark/controllers/FirstLaunchViewController.swift
@@ -7,7 +7,49 @@
 //
 
 import UIKit
+import CoreData
 
 class FirstLaunchViewController:UIViewController {
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var adress: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var birthdate: UIDatePicker!
+    @IBOutlet weak var register: UIButton!
     
+    func formIsValid(_ tab:[UITextField]!) -> Bool {
+        var flag: Bool = false
+        if let t = tab {
+            flag = (t.count == filterValues(tab).count)
+        }
+        return flag
+    }
+    
+    func filterValues(_ inputs:[UITextField]) -> [UITextField]! {
+        return inputs.filter{(item) in !(item.text?.isEmpty ?? true)}
+    }
+    
+    func createPatient() throws -> Patient {
+        let tab: [UITextField] = [lastName, firstName, adress, phoneNumber]
+        if formIsValid(tab) {
+            let patient: Patient = Patient(context : CoreDataManager.context)
+            patient.nom_patient = lastName.text
+            patient.prenom_patient = firstName.text
+            //patient.date_naissance = birthdate.date
+            patient.adresse = Adresse(context: CoreDataManager.context)
+            patient.telephone = phoneNumber.text
+            do {
+                try CoreDataManager.save()
+                return patient
+            }
+            catch let error as NSError {
+                throw error
+            }
+        }
+        else {
+            let nilValues = filterValues(tab)
+            throw Error(nilValues)
+        }
+    }
+
 }
---
+++ iPark/iPark/models/PatientModel.swift
@@ -0,0 +1,13 @@
+//
+//  PatientModel.swift
+//  iPark
+//
+//  Created by Johan BRUNET on 12/03/2018.
+//  Copyright © 2018 Jade HENNEBERT. All rights reserved.
+//
+
+import Foundation
+
+extension Patient {
+    
+}
