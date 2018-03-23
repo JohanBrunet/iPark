6d09790dd6f0aa51d7aecc981af91261eeb9c5b7
Johan Brunet <brunet.johan.23@gmail.com>
2018-03-23 11:21:12 +0100

    Use UserDefaults to store patient info

--- iPark/iPark/controllers/FirstLaunchViewController.swift
+++ iPark/iPark/controllers/FirstLaunchViewController.swift
@@ -29,18 +29,14 @@ class FirstLaunchViewController: UIViewController {
         let tab: [UITextField] = [lastName, firstName, adress]
         if formIsValid(tab) {
             let date:NSDate? = birthdate.date as NSDate?
-            do {
-                let patient: PatientModel = try PatientModel(nom: lastName.text!, prenom: firstName.text!, adresse: adress.text!, date_naissance: date)
-                UserDefaults.standard.set(true, forKey: "launchedBefore")
-                let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
-                let view = storyboard.instantiateViewController(withIdentifier: "NavController") as! UINavigationController
-                self.present(view, animated: true, completion: nil)
-            }
-            catch {
-                let alert = UIAlertController(title: "Erreur insertion", message: "Erreur lors de l'insertion des données", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Annuler", style: .cancel, handler: nil))
-                self.present(alert, animated: true, completion: nil)
-            }
+            UserDefaults.standard.set(lastName.text!, forKey: "nom")
+            UserDefaults.standard.set(firstName.text!, forKey: "prenom")
+            UserDefaults.standard.set(adress.text!, forKey: "adresse")
+            UserDefaults.standard.set(date, forKey: "date_naissance")
+            UserDefaults.standard.set(true, forKey: "launchedBefore")
+            let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+            let view = storyboard.instantiateViewController(withIdentifier: "NavController") as! UINavigationController
+            self.present(view, animated: true, completion: nil)
         }
         else {
             let alert = UIAlertController(title: "Formulaire invalide", message: "Veuillez remplir tous les champs", preferredStyle: .alert)
--- iPark/iPark/controllers/HomeViewController.swift
+++ iPark/iPark/controllers/HomeViewController.swift
@@ -13,15 +13,7 @@ class HomeViewController:UIViewController {
     @IBOutlet weak var usernameLabel: UILabel!
     
     override func viewDidLoad() {
-        let patient: PatientModel
-        do {
-            patient = try PatientModel()
-            usernameLabel.text = patient.prenom + " " + patient.nom
-        }
-        catch {
-            let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
-            let nav = storyboard.instantiateViewController(withIdentifier: "firstLaunchView") as! FirstLaunchViewController
-            self.present(nav, animated: true, completion: nil)
-        }
+        usernameLabel.text = UserDefaults.standard.string(forKey: "prenom")! + " " + UserDefaults.standard.string(forKey: "nom")!
     }
+
 }
