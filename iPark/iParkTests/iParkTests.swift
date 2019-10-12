2e88aa8b66b669eac57f0ef00c418353ef948658
ceccs17d55 <43598233+ceccs17d55@users.noreply.github.com>
2019-10-12 10:04:51 +0530

    Added blank spaces

--- iPark/iParkTests/iParkTests.swift
+++ iPark/iParkTests/iParkTests.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2018 Jade HENNEBERT. All rights reserved.
 //
 
+
+
+
 import XCTest
 @testable import iPark
 
@@ -21,15 +24,21 @@ class iParkTests: XCTestCase {
         super.tearDown()
     }
     
+    
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
+    
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
+            
+            
         }
     }
     
